2e07e31b6828e160be70f168bfc3fd4aaa3c2295
21Ayush21 <121169673+21Ayush21@users.noreply.github.com>
2023-08-11 00:49:24 +0530

    Made login functionality

--- minorproject/vlab/urls.py
+++ minorproject/vlab/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from . import views 
+from .views import login
 
 
 urlpatterns = [
     path('',views.hello , name='hello'),
+    path('login/',login.as_view(), name='login'),
 ]
--- minorproject/vlab/views.py
+++ minorproject/vlab/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def hello(request):
     return render(request , 'vlab/main.html')
+
+class login(LoginView):
+    template_name = 'vlab/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('hello')
