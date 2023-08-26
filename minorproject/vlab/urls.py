2e80d3697eba8670a69df6bbec0c93549f86f931
21Ayush21 <121169673+21Ayush21@users.noreply.github.com>
2023-08-26 20:23:11 +0530

    further changes

--- minorproject/vlab/urls.py
+++ minorproject/vlab/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views 
 from .views import login
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('',views.hello , name='hello'),
+    path('logout/', LogoutView.as_view(next_page='login') , name='logout'),
     path('login/',login.as_view(), name='login'),
     path('Theory/',views.Theory , name='Theory'),
 ]
