6833c8fba196ed83f0dd07e19b695d5ac7990332
21Ayush21 <121169673+21Ayush21@users.noreply.github.com>
2023-09-01 19:44:38 +0530

    Major changes
    added static folder for css and js files, created user model

--- minorproject/vlab/models.py
+++ minorproject/vlab/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=120)
+    
 
