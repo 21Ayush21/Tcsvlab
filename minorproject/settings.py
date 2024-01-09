e9cd793142ba6aa669f3fadcf7fd83eaea5494fe
21Ayush21 <bhandariayush21@gmail.com>
2024-01-09 19:03:01 +0530

    updated settings

--- minorproject/settings.py
+++ minorproject/settings.py
@@ -111,18 +111,18 @@ WSGI_APPLICATION = 'minorproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# import dj_database_url
-
 # DATABASES = {
-#     'default': dj_database_url.parse(env('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
 
 USE_X_FORWARDED_HOST = True
 
